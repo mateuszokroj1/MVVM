ce2504cb7cdd5ac770c866ffed2514061ec98e4e
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-11-22 00:25:37 +0100

    Command class created.

---
+++ Sources/Core/Command.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Mvvm
+{
+    /// <summary>
+    /// Basic command that invokes delegate when can be executed.
+    /// </summary>
+    public class Command : ICommand
+    {
+        #region Constructors
+
+        public Command()
+        {
+
+        }
+
+        protected Command() { }
+
+        #endregion
+
+        #region Fields
+
+        public event EventHandler CanExecuteChanged;
+        private readonly Action<object> toExecute;
+        private readonly 
+
+        #endregion
+
+        #region Methods
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
