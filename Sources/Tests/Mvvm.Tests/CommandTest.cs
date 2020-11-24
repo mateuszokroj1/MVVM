0c68087f6a3ae0d865ff78da6fd34e0d9a9cd21a
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-11-24 15:12:48 +0100

    Added Helpers dir in Tests. Created Command class test.

---
+++ Sources/Tests/Mvvm.Tests/CommandTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Mvvm.Tests
+{
+    public class CommandTest
+    {
+        #region Helper fields
+
+        private static readonly Func<object, bool> canExecute1_null = null;
+        private static readonly Func<bool> canExecute2_null = null;
+        private static readonly Action<object> toExecute1_null = null;
+        private static readonly Action toExecute2_null = null;
+
+        private static readonly Func<object, bool> canExecute1_valid = null;
+        private static readonly Func<bool> canExecute2_valid = null;
+        private static readonly Action<object> toExecute1_valid = null;
+        private static readonly Action toExecute2_valid = null;
+
+        #endregion
+
+        #region Constructors
+
+        [Fact]
+        public void Constructor1_WhenArgumentsAreNull__ShouldThrowNullReference()
+        {
+            Assert.Throws<NullReferenceException>(() => new Command(canExecute1_null, toExecute1_null));
+        }
+
+        [Fact]
+        public void Constructor1_WhenArgumentsAreValid_ShouldSet()
+        {
+
+        }
+
+        [Fact]
+        public void Constructor1_When_Should()
+        {
+
+        }
+
+        [Fact]
+        public void Constructor1_When_Should()
+        {
+
+        }
+
+        [Fact]
+        public void Constructor1_When_Should()
+        {
+
+        }
+
+        [Fact]
+        public void Constructor1_When_Should()
+        {
+
+        }
+
+        #endregion
+    }
+}
