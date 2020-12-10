00f79183c3dec7c74c31890c401d30cf3b84a691
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-12-10 18:21:43 +0100

    Completed tests for RaisePropertyChanged method. Created tests for RaisePropertiesChanged method.

---
+++ Sources/Tests/Mvvm.Tests/ViewModel/RaisePropertiesChanged.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+using Mvvm.Tests.Helpers;
+
+namespace Mvvm.Tests.ViewModel.Methods
+{
+    public class RaisePropertiesChanged
+    {
+        #region Constructor
+
+        public RaisePropertiesChanged()
+        {
+            this.propertyChangedObservable =
+                Observable
+                .FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>
+                (
+                    handler => this.model.PropertyChanged += handler,
+                    handler => this.model.PropertyChanged -= handler
+                )
+                .Select(args => args?.EventArgs?.PropertyName);
+        }
+
+        #endregion
+
+        #region Fields
+
+        private readonly DemoModel model = new DemoModel();
+        private readonly IObservable<string> propertyChangedObservable;
+
+        private readonly string[] raise2_nullArgument1 = null;
+        private readonly string[] raise2_nullArgument2 = new string[] { null, "TEST1", "", "   54$%( " };
+
+        #endregion
+
+        public void 
+
+        var expectedResult = raise2_nullArgument1.Where(value => !string.IsNullOrEmpty(value));
+
+        List<string> resultlist = new List<string>();
+
+            this.propertyChangedObservable.Subscribe(value => resultlist.Add(value));
+
+            this.model.Raise2(raise2_nullArgument1);
+
+            Assert.Equal(expectedResult.Count(), resultlist.Count);
+
+            foreach (var expectedValue in expectedResult)
+                if (!resultlist.Contains(expectedValue))
+                    throw new AssertActualExpectedException(expectedResult, resultlist, "Collections are not equal");
+    }
+}
--- Sources/Tests/Mvvm.Tests/ViewModel/RaisePropertyChanged.cs
+++ Sources/Tests/Mvvm.Tests/ViewModel/RaisePropertyChanged.cs
@@ -1,15 +1,63 @@
-﻿using Xunit;
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+
+using Mvvm.Tests.Helpers;
+
+using Xunit;
 
 namespace Mvvm.Tests.ViewModel.Methods
 {
     public class RaisePropertyChanged
     {
-        [Fact]
-        public void Variant1_WhenArgumentIsNullOrEmpty_ShouldDoNothing()
-        {
+        #region Constructor
 
+        public RaisePropertyChanged()
+        {
+            this.propertyChangedObservable =
+                Observable
+                .FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>
+                (
+                    handler => this.model.PropertyChanged += handler,
+                    handler => this.model.PropertyChanged -= handler
+                )
+                .Select(args => args?.EventArgs?.PropertyName);
         }
 
-        
+        #endregion
+
+        #region Fields
+
+        private readonly DemoModel model = new DemoModel();
+        private readonly IObservable<string> propertyChangedObservable;
+
+        #endregion
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void WhenArgumentIsNull_ShouldDoNothing(string testValue)
+        {
+            bool isInvoked = false;
+            this.propertyChangedObservable.Where(name => name == testValue).Subscribe(n => isInvoked = true);
+
+            this.model.Raise1(testValue);
+
+            Assert.False(isInvoked);
+        }
+
+        [Theory]
+        [InlineData("Test1")]
+        [InlineData(" ftey165%^ ")]
+        [InlineData("@#89   ")]
+        public void WhenArgumentIsNotEmpty_ShouldRaiseEvent(string testValue)
+        {
+            bool isInvoked = false;
+            this.propertyChangedObservable.Where(name => name == testValue).Subscribe(n => isInvoked = true);
+
+            this.model.Raise1(testValue);
+
+            Assert.True(isInvoked);
+        }
     }
 }
