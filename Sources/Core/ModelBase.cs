69455652bfeff9c28251cfe2e9b0132e49272b8c
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-12-12 00:19:46 +0100

    Added documentation to all public elements of ModelBase class.

--- Sources/Core/ModelBase.cs
+++ Sources/Core/ModelBase.cs
@@ -20,14 +20,14 @@ namespace Mvvm
         #region Methods
 
         /// <summary>
-        /// 
+        /// Raises PropertyChanged event. Use it to notify about changes in properties.
         /// </summary>
-        /// <param name="propertyName"></param>
+        /// <param name="propertyName">Name of changed property</param>
         protected void RaisePropertyChanged([CallerMemberName] string propertyName = "") =>
             RaisePropertiesChanged(propertyName);
 
         /// <summary>
-        /// 
+        /// Raises PropertyChanged event for many elements. Use it to notify about changes in properties.
         /// </summary>
         /// <param name="propertiesNames"></param>
         protected void RaisePropertiesChanged(params string[] propertiesNames)
@@ -39,22 +39,22 @@ namespace Mvvm
         }
 
         /// <summary>
-        /// 
+        /// Sets value in <paramref name="destination"/> when is not equal to <paramref name="newValue"/> and notify about changes in property.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="destination"></param>
-        /// <param name="newValue"></param>
-        /// <param name="propertyName"></param>
+        /// <typeparam name="T">Type to set</typeparam>
+        /// <param name="destination">Field to set</param>
+        /// <param name="newValue">Source value</param>
+        /// <param name="propertyName">Name of property that was changed</param>
         protected void SetPropertyAndNotify<T>(ref T destination, T newValue, [CallerMemberName] string propertyName = "") =>
             SetPropertyAndNotifyMany(ref destination, newValue, propertyName);
 
         /// <summary>
-        /// 
+        /// Sets value in <paramref name="destination"/> when is not equal to <paramref name="newValue"/> and notify about changes in properties.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="destination"></param>
-        /// <param name="newValue"></param>
-        /// <param name="propertiesNames"></param>
+        /// <typeparam name="T">Type to set</typeparam>
+        /// <param name="destination">Field to set</param>
+        /// <param name="newValue">Source value</param>
+        /// <param name="propertiesNames">Names of properties that was changed</param>
         protected void SetPropertyAndNotifyMany<T>(ref T destination, T newValue, params string[] propertiesNames)
         {
             if(!Equals(destination, newValue))
